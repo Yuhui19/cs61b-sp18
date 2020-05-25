8574cae9aae5322f4991f44452883dc6297858ba
Yuhui19 <yuhui.zhang@utah.edu>
2020-05-24 22:30:27 -0600

    lab3

--- lab3/IntList/IntListTest.java
+++ lab3/IntList/IntListTest.java
@@ -66,8 +66,20 @@ public class IntListTest {
         assertEquals(IntList.of(1, 2, 3), A);
     }
 
+    @Test(timeout = 1000)
+    public void testReverse(){
+        IntList A = IntList.of(1, 2, 3, 4);
+        IntList B = IntList.of(4, 3, 2, 1);
+        IntList C = IntList.of();
+        assertEquals(IntList.reverse(A), B);
+        assertNotEquals(IntList.reverse(B), B);
+        assertEquals(IntList.reverse(C), null);
+    }
+
     /** If you're running this from the command line, you'll need
       * to add a main method. See ArithmeticTest.java for an
       * example. */
+
+
 
 }
